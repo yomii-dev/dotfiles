b84f24e70ab4295ab67c2cdd9b4f4196ed6edb74
yomii-dev <gabgonzaga28@gmail.com>
2026-09-24 16:58:34 +0800

    chore: moved nnn stuff to separate files

---
+++ config/.config/fish/nnn.fish
@@ -0,0 +1,5 @@
+# nnn ENV variables
+set -gx NNN_FIFO '/tmp/nnn.fifo'
+set -gx NNN_PLUG 'p:preview-tui;d:dragdrop'
+set -gx NNN_PREVIEWDIR "$HOME/.cache/nnn/previews"
+set -gx NNN_PREVIEWIMGPROG chafa
